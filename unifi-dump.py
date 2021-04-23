6c5dbeda20ca5d67647c6777e4248dc576abc5fb
c0psrul3 <c0psrul3@pm.me>
2021-04-23 18:48:12 -0400

    made dump script more pretty

--- unifi-dump.py
+++ unifi-dump.py
@@ -14,7 +14,7 @@ apiendpoint = os.environ.get('API_URL', 'https://localhost:8443')
 apiusername = os.environ.get('API_USERNAME', 'ubnt')
 apipassword = os.environ.get('API_PASSWORD', 'ubnt')
 
-unifi = unifi.UniFi(apiendpoint, apiusername, apipassword)
+u = unifi.UniFi(apiendpoint, apiusername, apipassword)
 
 
 # Just as a help really, code will not be reached as config.get() will blow up
@@ -23,15 +23,24 @@ if apiendpoint is None:
 if apiusername is None or apipassword is None:
     raise AssertionError('API/Username and API/Password is required in configuration')
 
-print("SITES:")
-sites = unifi.api_get('self/sites')
-pprint.pprint(sites)
+# print controller status
+status = u.api_get('status')
+print("CONTROLLER STATUS: ", status.get('meta').get('rc'))
+print()
 
-for site in unifi.sites():
+sites = u.api_get('self/sites')
+if 'data' in sites:
+    print("SITES:")
+    for s in sites.get('data'):
+        print('\t', s.get('desc'), s.get('name'), s.get('_id'))
+        print()
+
+for site in u.sites():
     print('SITE: ' + site.name)
 
     print("DEVICES:")
-    devices = site.unifi.api_get(site.api_endpoint('stat/device'))
+    devices = u.api_get(site.api_endpoint('stat/device'))
+    #devices = site.u.api_get(site.api_endpoint('stat/device'))
     pprint.pprint(devices)
 
 print("STA:")
