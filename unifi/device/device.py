8e15686c575445b1abcc153fbeadec15aa2196b5
c0psrul3 <c0psrul3@pm.me>
2021-04-26 16:29:36 -0400

    add device object attributes to satisfy summary metric unifi_device_info{}

--- unifi/device/device.py
+++ unifi/device/device.py
@@ -1,13 +1,29 @@
 class Device(object):
     def __init__(self, site, data):
         self.site = site
-        self.name = data['name']
-        self.model = data['model']
-        self.id = data['_id']
-        self.device_id = data['device_id']
-        self.adopted = data['adopted']
-        self.mac = data['mac']
-        self.serial = data['serial']
+        self.name = data.get('name', "")
+        self.type = data.get('type', "")
+        self.model = data.get('model', "")
+        self.id = data.get('_id', "")
+        self.device_id = data.get('device_id', "")
+        self.adopted = str(data.get('adopted', 0))
+        self.ip_address = str(data.get('ip',""))
+        self.hostname = str(data.get('hostname',""))
+        self.mac = str(data.get('mac', ""))
+        self.serial = str(data.get('serial', ""))
+        self.version = str(data.get('version', ""))
+        self.upgradable = str(data.get('upgradable', ""))
+        self.architecture = str(data.get('architecture', ""))
+        self.cfgversion = str(data.get('cfgversion', ""))
+        self.kernel_version = str(data.get('kernel_version', ""))
+        self.board_rev = str(data.get('board_rev', ""))
+        self.provisioned_at = str(data.get('provisioned_at', ""))
+        self.last_seen = str(data.get('last_seen', ""))
+        self.connected_at = str(data.get('connected_at', ""))
+        self.uptime = str(data.get('uptime', ""))
+        self.overheating = str(data.get('overheating', ""))
+        self.power_source = str(data.get('power_source', ""))
+        self.power_source_voltage = str(data.get('power_source_voltage', ""))
         self.uplink = None
         self.sysstat = None
         self.stat = None
@@ -20,3 +36,4 @@ class Device(object):
 
     def parse_stat(self, data):
         self.stat = data
+
