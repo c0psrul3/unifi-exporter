0a8b6f060a9d98afde37b1114614f7465c92dcad
c0psrul3 <c0psrul3@pm.me>
2021-04-29 21:04:34 -0400

    updated unifi-dump.py after re-arrangement

--- unifi-dump.py
+++ unifi-dump.py
@@ -4,7 +4,7 @@ import time
 import os
 import argparse
 import pprint
-from unifi import unifi
+import unifi
 
 parser = argparse.ArgumentParser(description='UniFi Prometheus dump')
 
@@ -14,7 +14,7 @@ apiendpoint = os.environ.get('API_URL', 'https://localhost:8443')
 apiusername = os.environ.get('API_USERNAME', 'ubnt')
 apipassword = os.environ.get('API_PASSWORD', 'ubnt')
 
-u = unifi.UniFi(apiendpoint, apiusername, apipassword)
+u = unifi.Network(apiendpoint, apiusername, apipassword)
 
 
 # Just as a help really, code will not be reached as config.get() will blow up
@@ -27,6 +27,8 @@ if apiusername is None or apipassword is None:
 status = u.api_get('status')
 print("CONTROLLER STATUS: ", status.get('meta').get('rc'))
 print()
+
+pprint.pprint(u.api_get('stat/sites'))
 
 sites = u.api_get('self/sites')
 if 'data' in sites:
@@ -43,7 +45,18 @@ for site in u.sites():
     #devices = site.u.api_get(site.api_endpoint('stat/device'))
     pprint.pprint(devices)
 
-print("STA:")
-sta = site.sta()
-pprint.pprint(sta)
+    print("\nHEALTH:")
+    for health in site.health():
+        subsystem = health.pop('subsystem')
+        if health.get('status') != "unknown":
+            print(f"Subsystem: {subsystem}")
+            pprint.pprint(health)
+            print()
+    
+    print("STA:")
+    sta = site.sta()
+    pprint.pprint(sta)
+
+
+
 exit(0)
