8335f10af389e8bdfe609f4e78a5e49a126a18ee
c0psrul3 <c0psrul3@pm.me>
2021-04-29 21:05:03 -0400

    re-ordered class definition in network.py

--- unifi/network.py
+++ unifi/network.py
@@ -8,57 +8,6 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning, SNIMiss
 
 from . import device
 
-class Site(object):
-
-    def __init__(self, unifi, data):
-        self.unifi = unifi
-        self.id = data['_id']
-        self.desc = data['desc']
-        self.name = data['name']
-        self.role = data['role']
-
-    def api_endpoint(self, endpoint):
-        return 's/' + self.name + '/' + endpoint
-
-    def device(self):
-        # https://hemma:8443/api/s/default/stat/device
-
-        data = self.unifi.api_get(self.api_endpoint('stat/device'))
-        ret = []
-        for d in data['data']:
-            if d['model'] == 'UGW3':
-                ret.append(device.UGW3(self, d))
-            elif d['model'] == 'US8P150':
-                ret.append(device.US8P150(self, d))
-            elif d['model'] == 'USL16LP':
-                ret.append(device.USL16LP(self, d))
-            elif d['model'] == 'USL8LP':
-                ret.append(device.USL8LP(self, d))
-            elif d['model'] == 'USMINI':
-                ret.append(device.USMINI(self, d))
-            elif d['model'] == 'USF5P':
-                ret.append(device.USF5P(self, d))
-            elif d['model'] == 'U7PG2':
-                ret.append(device.U7PG2(self, d))
-            elif d['model'] == 'U7HD':
-                ret.append(device.U7HD(self, d))
-            elif d['model'] == 'U7LT':
-                ret.append(device.U7LT(self, d))
-            elif d['model'] == 'U7NHD':
-                ret.append(device.U7NHD(self, d))
-            elif d['model'] == 'UHDIW':
-                ret.append(device.UHDIW(self, d))
-            else:
-                print(f"Unknown device type/model: {d['type']}/{d['model']}")
-
-        return ret
-
-    def sta(self):
-        # https://hemma:8443/api/s/default/stat/sta
-        data = self.unifi.api_get(self.api_endpoint('stat/sta'))
-        return data['data']
-
-
 class Network(object):
     def __init__(self, addr, username, password):
         self.addr = addr
@@ -126,10 +75,9 @@ class Network(object):
                 raise e
 
 
-
     def set_error(self, r):
         if r.status_code != 200:
-            print("ERROR - Status Code: ", status_code)
+            print("ERROR - Status Code: ", r.status_code)
             return
         data = r.json()
         if 'meta' in data:
@@ -140,8 +88,9 @@ class Network(object):
             else:
                 raise UniFiException(None, 'FAIL: \n' + pprint.pformat(data))
 
+
     def login(self):
-        # https://hemma:8443/api/login
+        # https://controller:8443/api/login
         # > POST {"username":"ubnt","password":"ubnt","strict":true}:
         # < Set-Cookie: unifises=k8U3umwhciVfp8e43evU95mwQI3eAxK3; Path=/; Secure; HttpOnly
         # < Set-Cookie: csrf_token=k8U3umwhciVfp8e43evU95mwQI3eAxK3; Path=/; Secure
@@ -150,12 +99,82 @@ class Network(object):
         payload = { 'username': self.username, 'password': self.password }
         self.api_post('login', payload)
 
+
     def sites(self):
-        # https://hemma:8443/api/self/sites
+        # https://controller:8443/api/self/sites
         # { "data" : [ { "_id" : "56c87bc1b41038d25762ce86" , "attr_hidden_id" : "default" , "attr_no_delete" : true , "desc" : "Default" , "name" : "default" , "num_ap" : 2 , "num_sta" : 22 , "role" : "admin"}] , "meta" : { "rc" : "ok"}}
         data = self.api_get('self/sites')
         ret = []
         for s in data.get('data'):
             ret.append(Site(self, s))
         return ret
+
+
+class Site(object):
+    def __init__(self, unifi, data):
+        self.unifi = unifi
+        self.id = data['_id']
+        self.desc = data['desc']
+        self.name = data['name']
+        self.role = data['role']
+
+    def api_endpoint(self, endpoint):
+        return 's/' + self.name + '/' + endpoint
+
+    def health(self):
+        # https://controller:8443/api/s/default/stat/device
+        data = self.unifi.api_get(self.api_endpoint('stat/health'))
+        return data['data']
+
+
+    def device(self):
+        # https://controller:8443/api/s/default/stat/device
+
+        data = self.unifi.api_get(self.api_endpoint('stat/device'))
+        ret = []
+        for d in data['data']:
+            if d['model'] == 'UGW3':
+                ret.append(device.UGW3(self, d))
+            elif d['model'] == 'US8P150':
+                ret.append(device.US8P150(self, d))
+            elif d['model'] == 'USL16LP':
+                ret.append(device.USL16LP(self, d))
+            elif d['model'] == 'USL8LP':
+                ret.append(device.USL8LP(self, d))
+            elif d['model'] == 'USMINI':
+                ret.append(device.USMINI(self, d))
+            elif d['model'] == 'USF5P':
+                ret.append(device.USF5P(self, d))
+            elif d['model'] == 'U7PG2':
+                ret.append(device.U7PG2(self, d))
+            elif d['model'] == 'U7HD':
+                ret.append(device.U7HD(self, d))
+            elif d['model'] == 'U7LT':
+                ret.append(device.U7LT(self, d))
+            elif d['model'] == 'U7NHD':
+                ret.append(device.U7NHD(self, d))
+            elif d['model'] == 'UHDIW':
+                ret.append(device.UHDIW(self, d))
+            else:
+                print(f"Unknown device type/model: {d['type']}/{d['model']}")
+        return ret
+
+    def sta(self):
+        # https://controller:8443/api/s/default/stat/sta
+        data = self.unifi.api_get(self.api_endpoint('stat/sta'))
+        return data['data']
+
+
+
+class UniFiException(Exception):
+    apimsg = None
+
+    def __init__(self, apimsg, s=None):
+        m = s
+        if m is None:
+            m = apimsg
+        super(UniFiException, self).__init__(m)
+
+        self.apimsg = apimsg
+
 
